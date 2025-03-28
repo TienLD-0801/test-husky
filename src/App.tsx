7481db3d647ad43371f8abb82e963c4699e08446
tienld <tienld@bnksolution.com>
2025-03-28 23:52:12 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(32)
-  const sdk = '12';
-  console.log(123);
-  
+  const [count, setCount] = useState(32);
+  // const sdk = '12';
+  // console.log(123);
+
   return (
     <>
       <div>
@@ -31,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
